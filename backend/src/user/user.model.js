b2cd24feae1c083be95f71e06b9035a9d0dd69b3
alex <changli@steelkiwi.com>
2017-03-05 22:03:58 +0200

    Complex index for user model

--- backend/src/user/user.model.js
+++ backend/src/user/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  googleId: { type: String, required: true, unique: true },
+  googleId: { type: String, required: true, unique: true},
   token: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
@@ -14,16 +14,21 @@ const userSchema = new Schema({
   skillMarks: [{
     streamId: { type: mongoose.Schema.Types.ObjectId, ref: 'Stream', required: true },
     streamName: { type: String, required: true },
-    skillId: { type: mongoose.Schema.Types.ObjectId, ref: 'Skill', required: true },
+    skillId: { type: mongoose.Schema.Types.ObjectId, ref: 'Skill', required: true},
     skillName: { type: String, required: true },
     value: { type: Number, required: true, min: 0, max: 4 },
-    postedAt: { type: Date, default: Date.now, required: true, index: true },
+    postedAt: { type: Date, default: Date.now, required: true},
     approvement: {
-      postedAt: { type: Date},
-      approverGoogleId: { type: String},
-      approverName: { type: String}
+      postedAt: { type: Date },
+      approverGoogleId: { type: String },
+      approverName: { type: String }
     }
   }]
+});
+userSchema.index({
+  googleId: -1,
+  'skillMarks.skillId': -1,
+  'skillMarks.postedAt': -1
 });
 const User = mongoose.model('User', userSchema);
 
