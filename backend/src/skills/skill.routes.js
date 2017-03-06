5189f50be979af9bc0be742ba60cf6a1a3ac8c13
alex <changli@steelkiwi.com>
2017-03-06 23:50:39 +0200

    Refactor skill controller with promises

--- backend/src/skills/skill.routes.js
+++ backend/src/skills/skill.routes.js
@@ -12,17 +12,14 @@ module.exports = app => {
    */
   app.get('/api/v0/skills', isAuthenticatedAndHasPermissions([]), (request, response) => {
     const searchQuery = request.query.q;
-    Skill.find({
+    return Skill.find({
       name: {
         $regex: searchQuery ? searchQuery : '',
         $options: 'i'
       }
-    }, (error, skills) => {
-      if (error) {
-        return errorHandler(response, error);
-      }
-      return response.status(200).json(skills);
-    });
+    })
+      .then(foundSkills => response.status(200).json(foundSkills))
+      .catch(error => errorHandler(response, error));
   });
 
   /**
@@ -32,11 +29,8 @@ module.exports = app => {
    * `streamId` (object id string, required) - id of related stream
    */
   app.post('/api/v0/skills', isAuthenticatedAndHasPermissions(['skillComposer']), (request, response) => {
-    Skill.create(request.body, (error, created) => {
-      if (error) {
-        return errorHandler(response, error, 400);
-      }
-      return response.status(201).json(created);
-    });
+    return Skill.create(request.body)
+      .then(createdSkill => response.status(201).json(createdSkill))
+      .catch(error => errorHandler(response, error, 400));
   });
 };
