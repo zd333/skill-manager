c16655d2ac42e8fceda4b20cc2d8278e3177b3d8
alex <changli@steelkiwi.com>
2017-03-15 17:54:25 +0200

    Rename app component style file

--- spa/src/app/core/header/header.component.ts
+++ spa/src/app/core/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'skdsm-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.styl']
 })
 export class HeaderComponent implements OnInit {
 
