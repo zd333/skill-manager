26b950416c8eb194c153c1c809f02fc52c60fdb3
alex <changli@steelkiwi.com>
2017-03-20 02:13:52 +0200

    Something in progress

--- spa/src/app/skills/skills.module.ts
+++ spa/src/app/skills/skills.module.ts
@@ -1,9 +1,9 @@
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { SkillsService } from './skills.service';
 import { ManageSkillsComponent } from './manage-skills/manage-skills.component';
+import { AccordionModule } from 'ng2-bootstrap';
 
 @NgModule({
   imports: [
